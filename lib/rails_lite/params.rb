2b334d358f316c1bf709bdfba6a5d22a8f2f6908
Micah Buckley-Farlee <micahbuckleyfarlee@gmail.com>
2013-10-16 11:44:51 -0700

    Begin routing

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -3,6 +3,7 @@ require 'uri'
 class Params
   def initialize(req, route_params)
     @params = parse_www_encoded_form(req.query_string)
+    @params.merge!(parse_www_encoded_form(req.body))
   end
 
   def [](key)
@@ -16,9 +17,25 @@ class Params
   private
 
   def parse_www_encoded_form(www_encoded_form)
-    Hash[URI::decode_www_form(www_encoded_form)]
+    if www_encoded_form
+      flat_hash = Hash[URI::decode_www_form(www_encoded_form)]
+      form_hash = {}
+      flat_hash.map do |key, value| 
+        keys = parse_key(key)
+        last = form_hash
+        keys[0...-1].each do |hkey|
+          last[hkey] ||= {}
+          last = last[hkey]
+        end
+        last[keys.last] = value
+      end
+      form_hash
+    else
+      return {}
+    end
   end
 
   def parse_key(key)
+    key.split(/\]\[|\[|\]/)
   end
 end
--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -2,9 +2,15 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   def matches?(req)
+    req.request_method.downcase.to_sym == @http_method
+      && req.path =~ @pattern
   end
 
   def run(req, res)
